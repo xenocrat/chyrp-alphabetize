bca397babc4c9b8d89c561c9ffba821243446fe9
Daniel Pimley <dan@pimley.net>
2015-05-25 15:19:21 +0100

    Compatibility with Chyrp Lite v2015.05.25

---
+++ alphabetize/info.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "name"          => __("Alphabetize", "alphabetize"),
+    "url"           => "http://chyrplite.net/",
+    "version"       => 1.0,
+    "description"   => __("Sort the blog index alphabetically.", "alphabetize"),
+    "author"        => array(
+        "name"      => "Daniel Pimley",
+        "url"       => "http://www.pimley.net/")
+);
