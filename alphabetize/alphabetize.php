e67153d17c9417cb32ab68d80829b308c45a6c7c
Daniel Pimley <dan@pimley.net>
2022-08-27 12:49:16 +0100

    Code style changes

--- alphabetize/alphabetize.php
+++ alphabetize/alphabetize.php
@@ -2,10 +2,12 @@
     
     class Alphabetize extends Modules {
         public function main_alphabetical($main): void {
-            $results = SQL::current()->select("post_attributes",
-                                              "post_id, value",
-                                              array("name" => "title"),
-                                              array("post_id DESC"))->fetchAll();
+            $results = SQL::current()->select(
+                "post_attributes",
+                "post_id, value",
+                array("name" => "title"),
+                array("post_id DESC")
+            )->fetchAll();
 
             $ids = array();
 
@@ -15,25 +17,36 @@
             }
 
             if (!empty($ids)) {
-                $results = Post::find(array("placeholders" => true,
-                                            "where" => array("id" => $ids)));
+                $results = Post::find(
+                    array(
+                        "placeholders" => true,
+                        "where" => array("id" => $ids)
+                    )
+                );
 
-                usort($results[0], array($this, "sort_alphabetically"));
+                usort(
+                    $results[0],
+                    array($this, "sort_alphabetically")
+                );
                 $posts = new Paginator($results, $main->post_limit);
             } else {
                 $posts = new Paginator(array());
             }
 
-            $main->display(array("pages".DIR."alphabetical", "pages".DIR."index"),
-                           array("posts" => $posts),
-                           __("Posts in alphabetical order", "alphabetize"));
+            $main->display(
+                array("pages".DIR."alphabetical", "pages".DIR."index"),
+                array("posts" => $posts),
+                __("Posts in alphabetical order", "alphabetize")
+            );
         }
 
         private function sort_alphabetically($a, $b): int {
             $index_a = array_search("title", $a["attribute_names"]);
             $index_b = array_search("title", $b["attribute_names"]);
-            return $this->mb_strcasecmp($a["attribute_values"][$index_a],
-                                        $b["attribute_values"][$index_b]);
+            return $this->mb_strcasecmp(
+                $a["attribute_values"][$index_a],
+                $b["attribute_values"][$index_b]
+            );
         }
 
         private function mb_strcasecmp($str1, $str2, $encoding = "UTF-8"): int {
@@ -41,9 +54,16 @@
             $str2 = preg_replace("/[[:punct:]]+/", "", $str2);
 
             if (!function_exists("mb_strtoupper"))
-                return substr_compare(strtoupper($str1), strtoupper($str2), 0);
+                return substr_compare(
+                    strtoupper($str1),
+                    strtoupper($str2),
+                    0
+                );
 
-            return substr_compare(mb_strtoupper($str1, $encoding),
-                                  mb_strtoupper($str2, $encoding), 0);
+            return substr_compare(
+                mb_strtoupper($str1, $encoding),
+                mb_strtoupper($str2, $encoding),
+                0
+            );
         }
     }
