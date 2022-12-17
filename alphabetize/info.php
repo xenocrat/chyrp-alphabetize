4f06d02d7d0868f383b793c7fb99b14a627a2d90
Daniel Pimley <dan@pimley.net>
2022-12-17 13:59:55 +0000

    Fixed version string

--- alphabetize/info.php
+++ alphabetize/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Alphabetize", "alphabetize"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 1.2,
+    "version"       => "2023.01",
     "description"   => __("Sort the blog index alphabetically.", "alphabetize"),
     "author"        => array(
         "name"      => "Daniel Pimley",
